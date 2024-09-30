e800c4b4bbd8e6c815af1d71f34a122e752f7720
hackspark <hackspark@outlook.com>
2024-09-30 21:40:09 +0000

    Updated notes

--- Projects/LearnNode/parsejson/python/main.py
+++ Projects/LearnNode/parsejson/python/main.py
@@ -1,13 +1,13 @@
 import requests
 import json 
 
-url = 'http://localhost:3000'
+url = 'http://10.0.100.16:3000'
 
 data = {
     "name": "Corey Jones",
     "email": "cor3ytat@icloud.com"
 }
 
-response = request.post(url, json=data)
+response = requests.post(url, json=data)
 
 print(f'Response: {response.json}')
